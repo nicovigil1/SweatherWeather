f301a326350a0af5b576bac6c1f0955f46e52ad0
Nico <nicovigil1@gmail.com>
2019-02-24 15:39:55 -0700

    moves photo service spec to spec/services folder

---
+++ spec/services/photo_service_spec.rb
@@ -0,0 +1,20 @@
+require "rails_helper"
+
+describe "Photo Service" do
+  it 'can connect to flicker and respond with an image info', :vcr do 
+    lat_long = [39.74000930786133, -104.99201965332031]
+    service = PhotoService.new(lat_long)
+    expected = service.find_photo
+
+    expect(expected["id"]).to be_truthy
+  end 
+
+  it 'can generate a link to an image', :vcr do 
+    lat_long = [39.74000930786133, -104.99201965332031]
+    service = PhotoService.new(lat_long)
+
+    expected = service.generate_img_link
+    require 'pry'; binding.pry
+    expect(expected).to include("farm")
+  end 
+end
