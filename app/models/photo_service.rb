667baa4c9fd7216f1416252027bb593a37609615
Nico <nicovigil1@gmail.com>
2019-02-24 14:30:27 -0700

    adds fully tested & functioning image  service

--- app/models/photo_service.rb
+++ app/models/photo_service.rb
@@ -5,7 +5,7 @@ class PhotoService
     @long = coord[1]
     @conn = Faraday.new(url: "https://api.flickr.com") do |c|
       c.request :url_encoded
-      c.response :xml, parser_options: {symbolize_names: true}
+      c.response :xml
       c.adapter Faraday.default_adapter
     end 
   end
@@ -14,10 +14,21 @@ class PhotoService
     conn.get do |req|
       req.url("/services/rest/")
       req.params["api_key"] = ENV['FLICKER_KEY']
-      req.params["method"] = "flickr.places.findByLatLon"
-      req.params["lat"] = @lat 
+      req.params["method"] = "flickr.photos.search"
+      req.params["lat"] = @lat
       req.params["lon"] = @long
-      req.params["accuracy"] = 11 
-    end.body
+      req.params["sort"] = "interestingness-desc"
+      req.params["extras"] = "o_dims"
+      req.params["accuracy"] = 11
+    end.body["rsp"]["photos"]["photo"][0]
+  end
+
+  def generate_img_link
+    response = find_photo
+    farm_id = response["farm"]
+    server_id = response["server"]
+    id = response["id"]
+    secret = response["secret"]
+    "https://farm#{farm_id}.staticflickr.com/#{server_id}/#{id}_#{secret}.jpg"
   end 
 end
