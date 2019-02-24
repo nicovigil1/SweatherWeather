70ec171162f257e8183e40eceebbb384556ad5c0
Nico <nicovigil1@gmail.com>
2019-02-23 23:36:20 -0700

    adds passing test for location service coordinate generation

---
+++ app/models/location_service.rb
@@ -0,0 +1,25 @@
+class LocationService 
+  attr_reader :conn
+  def initialize
+    @conn = Faraday.new(url: "http://dev.virtualearth.net") do |conn|
+      conn.request :url_encoded
+      conn.response :json, parser_options: { symbolize_names: true }
+      conn.adapter Faraday.default_adapter
+    end
+  end
+
+  def find_coord(city, state)
+    response = conn.get do |req| 
+      req.url "/REST/v1/Locations/US/"
+      req.params["adminDistrict"] = state
+      req.params["locality"] = city
+      req.params["key"] = ENV["BING_KEY"]
+    end
+    sift_to_coordinates(response.body)
+  end
+
+  def sift_to_coordinates(hash)
+    hash[:resourceSets][0][:resources][0][:geocodePoints][0][:coordinates]
+  end
+end 
+  
