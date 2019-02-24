eae6a68fc0bd87fb2ad5e68c0f8efc037a43b48c
Nico <nicovigil1@gmail.com>
2019-02-23 21:24:14 -0700

    vcr setup

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -59,3 +59,10 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 end
+
+VCR.configure do |c|
+  c.cassette_library_dir = "spec/cassettes"
+  c.hook_into :webmock
+  c.configure_rspec_metadata!
+end
+
