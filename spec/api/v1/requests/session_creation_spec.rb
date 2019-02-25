a1af0a51e477437d3113b28e9155ecfd9c2eccd3
Nico <nicovigil1@gmail.com>
2019-02-25 08:07:17 -0700

    adds tests for login after registration spec

---
+++ spec/api/v1/requests/session_creation_spec.rb
@@ -0,0 +1,28 @@
+require "rails_helper"
+describe "Session Creation/Login" do
+  it 'can register with good credentials' do 
+    params = {
+      email: "example@example.com",
+      password: "password",
+      token: 123
+    }
+    User.create(params)
+    
+    post api_v1_sessions_path(params) 
+
+    expect(response.status).to eq(200)
+  end 
+
+  it 'can register with good credentials' do 
+    params = {
+      email: "example@example.com",
+      password: "password",
+    }
+    
+    post api_v1_sessions_path(params) 
+    
+    expect(response.status).to eq(401)
+    expect(User.all.length).to eq(0)
+  end 
+end
+  
