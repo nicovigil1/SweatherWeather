8f81c2c3e9fa04acbe90476d2f21a15f2d471d55
Nico <nicovigil1@gmail.com>
2019-02-25 15:00:02 -0700

    security refactor to the sessions controller

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:email])
-    if user
+    if user && user.authenticate(params[:password])
       render json: {key: user.token}, status: 200
     else
       render json: {error: "try again"}, status: 401
