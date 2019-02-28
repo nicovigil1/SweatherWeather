1a8bff7ebc6c301d22ab79c27757bc31ca1c7e4a
Nico <nicovigil1@gmail.com>
2019-02-27 18:22:11 -0700

    adds sad path testing for if a user tries to register twice

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,7 +2,11 @@ class Api::V1::UsersController < ApplicationController
   def create
     if passwords_match
       user = User.create(user_params)
-      render json: {key: user.generate_key}, status: 201
+      if user.token
+        render json: {key: user.generate_key}, status: 201
+      else
+        render json: {whoa: "there has already been a key generated for this account, please login at /sessions"}, status: 201
+      end
     else
       render json: {error: "try again"}, status: 401
     end 
